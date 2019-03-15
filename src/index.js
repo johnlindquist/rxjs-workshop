eb14ae4cdce22a98905cf7d1a977f0a467617225
John Lindquist <johnlindquist@gmail.com>
2019-03-15 11:31:37 -0600

    pr test

--- src/index.js
+++ src/index.js
@@ -1,52 +1,5 @@
-let consumer1 = {
-  update(value) {
-    if (!this.done) {
-      console.log("Consumer 1", value)
-    }
-  }
+function handle(value) {
+  console.log(value)
 }
 
-let consumer2 = {
-  update(value) {
-    if (!this.done) {
-      console.log("Consumer 2", value)
-    }
-  }
-}
-
-function connect(consumer) {
-  return {
-    done: false,
-    update(value) {
-      if (!this.done) {
-        consumer.update(value)
-      }
-    }
-  }
-}
-
-let producer = {
-  connections: [],
-
-  addConsumer(consumer) {
-    this.connections.push(connect(consumer))
-  },
-
-  update(value) {
-    this.connections.forEach(connection => {
-      connection.update(value)
-    })
-  },
-  done() {
-    this.connections.forEach(connection => {
-      connection.done = true
-    })
-  }
-}
-
-producer.addConsumer(consumer1)
-producer.addConsumer(consumer2)
-
-producer.update("John")
-producer.done()
-producer.update("Mindy")
+function go(value) {}
